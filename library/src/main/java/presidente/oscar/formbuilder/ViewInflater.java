4e497121cbba8e6a9d6478fa10ae552909f8b452
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2017-04-24 12:28:28 -0600

    Refactors view inflation based on the rules defined

--- library/src/main/java/presidente/oscar/formbuilder/ViewInflater.java
+++ library/src/main/java/presidente/oscar/formbuilder/ViewInflater.java
@@ -50,27 +50,22 @@ public class ViewInflater {
 
     public View inflateFromJson(JSONObject jsonObject, ViewGroup parent) {
         try {
-            String type = null;
-            if (jsonObject.has(Constants.VIEW_TYPE)) {
-                type = jsonObject.getString(Constants.VIEW_TYPE);
-            } else {
-                // No type so we just return null
+            // name and type are required
+            if (!jsonObject.has(Constants.VIEW_TYPE) || !jsonObject.has(Constants.VIEW_NAME)) {
                 return null;
             }
 
-            String name = null;
-            if (jsonObject.has(Constants.VIEW_NAME)) {
-                name = jsonObject.getString(Constants.VIEW_NAME);
-            }
+            String type = jsonObject.getString(Constants.VIEW_TYPE);
+            String name = jsonObject.getString(Constants.VIEW_NAME);
 
             ViewConfig config = new ViewConfig(name, type);
 
             View v = null;
             if (type.compareTo(Constants.TYPE_TEXT_INPUT) == 0) {
-                v = inflateTextArea(jsonObject, parent);
+                v = inflateTextInput(jsonObject, parent);
                 ((TextInputLayout)v).getEditText().setMaxLines(1);
             } else if (type.compareTo(Constants.TYPE_TEXT_AREA) == 0) {
-                v = inflateTextArea(jsonObject, parent);
+                v = inflateTextInput(jsonObject, parent);
             } else if (type.compareTo(Constants.TYPE_RADIO_GROUP) == 0) {
                 v = inflateRadioGroup(jsonObject, parent);
             } else if (type.compareTo(Constants.TYPE_CHECK_BOX) == 0) {
@@ -80,6 +75,7 @@ public class ViewInflater {
             if (v != null) {
                 v.setTag(config);
             }
+
             return v;
         } catch (JSONException e) {
             Util.logError(TAG, e.getMessage());
@@ -87,110 +83,158 @@ public class ViewInflater {
         }
     }
 
-    private View inflateCheckbox(JSONObject jsonObject, ViewGroup parent) {
-        return null;
-    }
+    private View inflateCheckbox(JSONObject jsonObject, ViewGroup parent) throws JSONException {
+        LinearLayout linearLayout = (LinearLayout) mLayoutInflater.inflate(R.layout.layout_checkbox, parent, false);
 
-    private View inflateTextArea(JSONObject jsonObject, final ViewGroup parent) throws JSONException {
-        TextInputLayout textInputLayout = (TextInputLayout) mLayoutInflater.inflate(R.layout.layout_textarea, parent, false);
+        TextView titleTv = (TextView) linearLayout.getChildAt(0);
 
-        final TextInputEditText textInput = (TextInputEditText) textInputLayout.getEditText();
+        // We know the index of the radio group
+        RadioGroup radioGroup = (RadioGroup) linearLayout.getChildAt(1);
 
-        String title = null;
         if (jsonObject.has(Constants.VIEW_PROPS)) {
             JSONObject props = jsonObject.getJSONObject(Constants.VIEW_PROPS);
 
             if (props.has(Constants.VIEW_PROPS_TITLE)) {
+                String title = props.getString(Constants.VIEW_PROPS_TITLE);
+                titleTv.setText(title);
+            }
+        }
 
+        if (jsonObject.has(Constants.VIEW_RADIO_OPTIONS)) {
+            JSONArray optionsArray = jsonObject.getJSONArray(Constants.VIEW_RADIO_OPTIONS);
+
+            for (int i = 0; i < optionsArray.length(); i++) {
+                JSONObject jsonOption = optionsArray.getJSONObject(i);
+
+                RadioButton option = (RadioButton) mLayoutInflater.inflate(R.layout.layout_radioubutton, radioGroup, false);
+
+                option.setId(i);
+
+                if (jsonOption.has(Constants.VIEW_RADIO_OPTIONS_VALUE)) {
+                    String optionId = jsonOption.getString(Constants.VIEW_RADIO_OPTIONS_VALUE);
+                    option.setTag(optionId);
+                }
+
+                if (jsonOption.has(Constants.VIEW_RADIO_OPTIONS_TITLE)) {
+                    String title = jsonOption.getString(Constants.VIEW_RADIO_OPTIONS_TITLE);
+                    option.setText(title);
+                }
+
+                radioGroup.addView(option);
+            }
+        }
+
+
+        return linearLayout;
+    }
+
+    private View inflateTextInput(JSONObject jsonObject, final ViewGroup parent) throws JSONException {
+
+        TextInputLayout textInputLayout = (TextInputLayout) mLayoutInflater.inflate(R.layout.layout_textarea, parent, false);
+        final TextInputEditText textInput = (TextInputEditText) textInputLayout.getEditText();
+
+        // Is it a input view or textarea?
+        String type = jsonObject.getString(Constants.VIEW_TYPE);
+
+        String title = null;
+
+        if (jsonObject.has(Constants.VIEW_PROPS)) {
+            JSONObject props = jsonObject.getJSONObject(Constants.VIEW_PROPS);
+
+            if (props.has(Constants.VIEW_PROPS_TITLE)) {
                 title = props.getString(Constants.VIEW_PROPS_TITLE);
-
-                Util.log(TAG, "title for textInput is " + title);
-
                 textInputLayout.setHint(title);
             }
         }
 
-        if (jsonObject.has(Constants.VIEW_CONFIG)) {
-            JSONObject config = jsonObject.getJSONObject(Constants.VIEW_CONFIG);
+        // If it's text are we don't set date or date time listeners
+        if (type.compareTo(Constants.TYPE_TEXT_INPUT) == 0) {
 
-            if (config.has(Constants.VIEW_CONFIG_TEXTINPUT_TYPE)) {
-                String type = config.getString(Constants.VIEW_CONFIG_TEXTINPUT_TYPE);
+            if (jsonObject.has(Constants.VIEW_CONFIG)) {
+                JSONObject config = jsonObject.getJSONObject(Constants.VIEW_CONFIG);
 
-                if (type.compareTo(Constants.INPUT_TYPE_TEXT) == 0) {
-                    textInput.setInputType(InputType.TYPE_CLASS_TEXT);
-                } else if (type.compareTo(Constants.INPUT_TYPE_NUMBER) == 0) {
-                    textInput.setInputType(InputType.TYPE_CLASS_NUMBER);
-                } else if (type.compareTo(Constants.INPUT_TYPE_PHONE) == 0) {
-                    textInput.setInputType(InputType.TYPE_CLASS_PHONE);
-                } else if (type.compareTo(Constants.INPUT_TYPE_DATETIME) == 0) {
-                    textInput.setFocusable(false);
-                    textInput.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            String timeSet = textInput.getText().toString();
+                if (config.has(Constants.VIEW_CONFIG_TEXTINPUT_TYPE)) {
+                    String inputType = config.getString(Constants.VIEW_CONFIG_TEXTINPUT_TYPE);
 
-                            Calendar calendar = Calendar.getInstance();
+                    if (inputType.compareTo(Constants.INPUT_TYPE_TEXT) == 0) {
+                        textInput.setInputType(InputType.TYPE_CLASS_TEXT);
+                    } else if (inputType.compareTo(Constants.INPUT_TYPE_NUMBER) == 0) {
+                        textInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    } else if (inputType.compareTo(Constants.INPUT_TYPE_PHONE) == 0) {
+                        textInput.setInputType(InputType.TYPE_CLASS_PHONE);
+                    } else if (inputType.compareTo(Constants.INPUT_TYPE_DATETIME) == 0) {
+                        textInput.setFocusable(false);
+                        textInput.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String timeSet = textInput.getText().toString();
 
-                            if (!timeSet.isEmpty()) {
-                                try {
-                                    Date date = Util.parseTime(timeSet);
-                                    calendar.setTime(date);
-                                } catch (ParseException e) {
-                                    Util.logError(TAG, e.getMessage());
+                                Calendar calendar = Calendar.getInstance();
+
+                                if (!timeSet.isEmpty()) {
+                                    try {
+                                        Date date = Util.parseTime(timeSet);
+                                        calendar.setTime(date);
+                                    } catch (ParseException e) {
+                                        Util.logError(TAG, e.getMessage());
+                                    }
                                 }
+
+                                TimePickerDialog dialog = new TimePickerDialog(parent.getContext(),
+                                        new TimePickerDialog.OnTimeSetListener() {
+                                            @Override
+                                            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                                                Calendar c = Calendar.getInstance();
+                                                c.set(Calendar.HOUR_OF_DAY, hour);
+                                                c.set(Calendar.MINUTE, minute);
+
+                                                textInput.setText(Util.formatTime(c.getTime()));
+                                            }
+                                        }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);
+
+                                dialog.show();
                             }
+                        });
+                    } else if (inputType.compareTo(Constants.INPUT_TYPE_DATE) == 0) {
+                        textInput.setFocusable(false);
+                        textInput.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String dateSet = textInput.getText().toString();
 
-                            TimePickerDialog dialog = new TimePickerDialog(parent.getContext(),
-                                    new TimePickerDialog.OnTimeSetListener() {
-                                        @Override
-                                        public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                                            Calendar c = Calendar.getInstance();
-                                            c.set(Calendar.HOUR_OF_DAY, hour);
-                                            c.set(Calendar.MINUTE, minute);
+                                Calendar calendar = Calendar.getInstance();
 
-                                            textInput.setText(Util.formatTime(c.getTime()));
-                                        }
-                                    }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);
-
-                            dialog.show();
-                        }
-                    });
-                } else if (type.compareTo(Constants.INPUT_TYPE_DATE) == 0) {
-                    textInput.setFocusable(false);
-                    textInput.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            String dateSet = textInput.getText().toString();
-
-                            Calendar calendar = Calendar.getInstance();
-
-                            if (!dateSet.isEmpty()) {
-                                try {
-                                    Date date = Util.parseDate(dateSet);
-                                    calendar.setTime(date);
-                                } catch (ParseException e) {
-                                    Util.log(TAG, e.getMessage());
+                                if (!dateSet.isEmpty()) {
+                                    try {
+                                        Date date = Util.parseDate(dateSet);
+                                        calendar.setTime(date);
+                                    } catch (ParseException e) {
+                                        Util.log(TAG, e.getMessage());
+                                    }
                                 }
+
+                                DatePickerDialog dialog = new DatePickerDialog(parent.getContext(), new DatePickerDialog.OnDateSetListener() {
+                                    @Override
+                                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                        Calendar c = Calendar.getInstance();
+                                        c.set(Calendar.YEAR, year);
+                                        c.set(Calendar.MONTH, month);
+                                        c.set(Calendar.DAY_OF_MONTH, day);
+
+                                        textInput.setText(Util.formatDate(c.getTime()));
+                                    }
+                                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+                                dialog.show();
                             }
-
-                            DatePickerDialog dialog = new DatePickerDialog(parent.getContext(), new DatePickerDialog.OnDateSetListener() {
-                                @Override
-                                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                                    Calendar c = Calendar.getInstance();
-                                    c.set(Calendar.YEAR, year);
-                                    c.set(Calendar.MONTH, month);
-                                    c.set(Calendar.DAY_OF_MONTH, day);
-
-                                    textInput.setText(Util.formatDate(c.getTime()));
-                                }
-                            }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-
-                            dialog.show();
-                        }
-                    });
+                        });
+                    }
                 }
             }
+        } else {
+            textInput.setInputType(InputType.TYPE_CLASS_TEXT);
         }
+
         return textInputLayout;
     }
 
@@ -222,8 +266,8 @@ public class ViewInflater {
                 option.setId(i);
 
                 if (jsonOption.has(Constants.VIEW_RADIO_OPTIONS_VALUE)) {
-                    String optionName = jsonOption.getString(Constants.VIEW_RADIO_OPTIONS_VALUE);
-                    option.setTag(optionName);
+                    String optionId = jsonOption.getString(Constants.VIEW_RADIO_OPTIONS_VALUE);
+                    option.setTag(optionId);
                 }
 
                 if (jsonOption.has(Constants.VIEW_RADIO_OPTIONS_TITLE)) {
